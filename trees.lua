8842ceb890b2d4678704001c78ac804f70ec22c8
Chloroxite <68760814+Chloroxite@users.noreply.github.com>
2023-04-16 15:07:11 -0500

    trees galore!

--- trees.lua
+++ trees.lua
@@ -1,22 +1,23 @@
 --Global configuration settings
 FarmPadding = 4 --Amount of blocks between tree positions.
 FarmSize = 4 -- Actual size of each row measured in tree slots
-sleepTime = 20
+SleepTime = 20
 
 local function harvest()
   turtle.dig()
   turtle.forward()
   turtle.digDown()
-  while(turtle.compareUp()) do
+  repeat
     turtle.digUp() 
     turtle.up() 
     if(turtle.getItemCount(2) < 63) then
       turtle.select(2)
       turtle.drop()
     end
-  end
+  until(!turtle.compareUp())
+
   os.sleep(5)
-  while(turtle.down()) do end
+  repeat until(!turtle.down())
   turtle.back()
 end
 
@@ -42,5 +43,5 @@ while(true) do
     end
   end
   reset(FarmSize)
-  os.sleep(sleepTime)
+  os.sleep(SleepTime)
 end
