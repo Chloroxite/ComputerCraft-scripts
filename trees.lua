ba8db687423c9a428b76a9642c94d57538a6ffa5
Chloroxite <68760814+Chloroxite@users.noreply.github.com>
2023-04-16 13:16:37 -0500

    updated trees.lua

--- trees.lua
+++ trees.lua
@@ -1,7 +1,8 @@
 --Global configuration settings
 FarmPadding = 4 --Amount of blocks between tree positions.
 FarmSize = 4 -- Actual size of each row measured in tree slots
-
+sleepTime = 20
+ 
 local function harvest()
   turtle.dig()
   turtle.forward()
@@ -17,13 +18,13 @@ local function harvest()
   while(turtle.down()) do end
   turtle.back()
 end
-
+ 
 local function reset(size)
-  for pos = 0, (FarmPadding-1)*size do
+  for pos = 0, FarmPadding*(size-1)-1 do
     turtle.back()
   end
 end
-
+ 
 while(true) do
   --Begin cycle
   for pos = 0, FarmSize-1 do
@@ -32,9 +33,13 @@ while(true) do
       harvest()
     end
     turtle.turnRight()
-    for i = 0, FarmPadding-1 do
-      turtle.forward()
-    end
+	if(pos ~= FarmSize-1) then
+    	for i = 0, FarmPadding-1 do
+			turtle.dig()
+      		turtle.forward()
+    	end
+	end
   end
   reset(FarmSize)
+  os.sleep(sleepTime)
 end
