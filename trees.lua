9540883db1bc5ef1a486dc007280d018b3e909fb
Chloroxite <68760814+Chloroxite@users.noreply.github.com>
2023-04-16 13:18:46 -0500

    updated trees.lua 2 electric boogaloo

--- trees.lua
+++ trees.lua
@@ -2,7 +2,7 @@
 FarmPadding = 4 --Amount of blocks between tree positions.
 FarmSize = 4 -- Actual size of each row measured in tree slots
 sleepTime = 20
- 
+
 local function harvest()
   turtle.dig()
   turtle.forward()
@@ -18,13 +18,13 @@ local function harvest()
   while(turtle.down()) do end
   turtle.back()
 end
- 
+
 local function reset(size)
   for pos = 0, FarmPadding*(size-1)-1 do
     turtle.back()
   end
 end
- 
+
 while(true) do
   --Begin cycle
   for pos = 0, FarmSize-1 do
@@ -35,10 +35,10 @@ while(true) do
     turtle.turnRight()
 	if(pos ~= FarmSize-1) then
     	for i = 0, FarmPadding-1 do
-			turtle.dig()
-      		turtle.forward()
+		  	turtle.dig()
+      	turtle.forward()
     	end
-	end
+	  end
   end
   reset(FarmSize)
   os.sleep(sleepTime)
